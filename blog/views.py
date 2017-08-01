47b196332aab6b27ee55d1fafd6b34ec614cd34a
Your Name <you@example.com>
2017-08-01 10:33:50 -0700

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post, Category 
 
 def category_list(request):
-    return render(request, 'blog/category_list.html', {})
+    categories = Category.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/category_list.html', {'categories': categories})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
