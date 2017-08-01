ac99401e6599e0d3bea8cb23b6ace8f9f27a48ab
Your Name <you@example.com>
2017-08-01 11:13:36 -0700

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post, Category 
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def category_list(request):
     categories = Category.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -18,3 +20,15 @@ def category_detail(request, pk):
     category = get_object_or_404(Category, pk=pk)
     return render(request, 'blog/post_detail.html', {'category': category})
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
