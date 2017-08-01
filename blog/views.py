882d8514f2c6ef87cddc8af8982843dddd614f32
Your Name <you@example.com>
2017-08-01 10:09:15 -0700

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def category_list(request):
+    return render(request, 'blog/category_list.html', {})
