57c35dc55472293b7ab3bfc7c84974437882878c
Your Name <you@example.com>
2017-08-01 10:59:33 -0700

    Added view and template for detailed blog post & category as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.category_list, name='category_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^category/(?P<pk>\d+)/$', views.category_detail, name='category_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'blog/post_detail.html', {'category': category})
+
